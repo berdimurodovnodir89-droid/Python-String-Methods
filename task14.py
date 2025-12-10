ae0336959472d5fa2e95a751591bdf12e46dd075
Nodirbek <berdimurodovnodir89@gmail.com>
2025-12-10 20:52:33 +0500

    task14 bajarildi

---
+++ task14.py
@@ -0,0 +1,14 @@
+# word = 'salom'
+# repeat = input('search :')
+# if repeat.upper() in word.upper() :
+#     print('True')
+# else :
+#     print('False')
+
+word = input('word :')
+
+word_end = input('wors_end :')
+
+result = word.upper() in word_end.upper()
+
+print(result)
